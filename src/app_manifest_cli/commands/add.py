fa89c0b75cc797580d901cd602a66cfc1749d135
borislavr <boris.lavr@gmail.com>
2025-08-29 17:54:18 +0300

    feat: improve add_command function to handle dependencies more effectively

--- src/app_manifest_cli/commands/add.py
+++ src/app_manifest_cli/commands/add.py
@@ -10,10 +10,17 @@ def add_command(
     configuration: str | None = typer.Option(None, "--config", "-c"),
     out: typer.FileTextWrite | None = typer.Option(None, "--out", "-o"),
 ):
-    payload_text = file.read() if file else (text if text is not None else sys.stdin.read())
+
     if section == "components":
+        payload_text = file.read() if file else (text if text is not None else sys.stdin.read())
         _add_component(manifest, payload_text, out)
     elif section == "dependencies":
-        _add_dependency(manifest, payload_text, configuration, out)
+        if configuration and (file or text):
+            raise typer.BadParameter("Only one of --config or --file/--text can be provided for dependencies")
+        if not configuration:
+            payload_text = file.read() if file else (text if text is not None else sys.stdin.read())
+            _add_dependency(manifest_path=manifest, payload_text=payload_text, configuration=None, out_file=out)
+        else:
+            _add_dependency(manifest_path=manifest, payload_text=None, configuration=configuration, out_file=out)
     else:
         raise typer.BadParameter("section must be 'components' or 'dependencies'")
